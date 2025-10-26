1ddcb76ce85344aa73dae2c25e0756eca83d09af
Daniyal <dkurbanov2009@gmail.com>
2025-10-26 17:49:59 +0300

    slider_notfinish

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
 new WOW().init();
 
-const Swiper = new Swiper ('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  
-  })
+
