aec0fef7b5db6d59349ce2520a2f39680493cc0d
Daniyal <dkurbanov2009@gmail.com>
2025-10-26 16:05:05 +0300

    swiper

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 new WOW().init();
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+  })
