8292a8c89fcd089f1dc7f1914bec2daa1c52bbac
Daniyal <dkurbanov2009@gmail.com>
2025-10-28 16:18:46 +0300

    sleder

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 new WOW().init();
 
 
+new Swiper('.swiper', {
+
+    // Optional parameters
+    loop: true,  
+    // Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  
+  });
